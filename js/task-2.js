2db6f1c8fd23c9af761f5aebea9a5078f14f7c39
olyvka <103537514+olyvka@users.noreply.github.com>
2023-11-28 00:52:10 +0200

    add task

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,18 @@
-function formatMessage(message, maxLength) {
-    if (message.length > maxLength) {
-        const newMessage = message.slice(0, maxLength) + `...`;
-        return newMessage;
+function makeArray(firstArray, secondArray, maxLength ){
+    let newArray = [];
+    newArray = firstArray.concat(secondArray);
+    if (newArray.length > maxLength) {
+        let newArrayMax = newArray.slice(0, maxLength)
+        return newArrayMax;
     } else {
-        return message;
+        return newArray;
     }
+
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc..."
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
